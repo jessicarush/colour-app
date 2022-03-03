99b421777481bc041452cad1edd664c79125b386
jessicarush <jessica@13down.io>
2022-03-02 21:27:34 -0800

    Style: minor formatting

--- src/experiments/TestComponent.js
+++ src/experiments/TestComponent.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import React, { Component } from "react";
+// import React, { Component } from 'react';
 import TestPureChild from "./TestPureChild";
 
 function TestComponent() {
